350c5aa7cef84197213801c9c5b44706648f3698
rutorl <130262541+rutorl@users.noreply.github.com>
2023-04-08 18:16:28 -0800

    Update forkserver.py

--- forkserver.py
+++ forkserver.py
@@ -13,7 +13,7 @@ from . import reduction
 from . import semaphore_tracker
 from . import spawn
 from . import util
-
+   
 __all__ = ['ensure_running', 'get_inherited_fds', 'connect_to_new_process',
            'set_forkserver_preload']
 
